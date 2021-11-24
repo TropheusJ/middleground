93c8e45fade5426439df3ae21ff1d3a5fd6435c5
TropheusJ <60247969+TropheusJ@users.noreply.github.com>
2021-11-23 20:02:46 -0500

    Initial commit

---
+++ src/main/java/io/github/tropheusj/modid/ExampleMod.java
@@ -0,0 +1,9 @@
+package io.github.tropheusj.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
